de0351c1c005810b528238c48fd50052a023b41d
Breno Duarte <brenoduarte239@gmail.com>
2024-03-12 19:29:00 -0300

    Commit

--- ListaEncadeada/main.c
+++ ListaEncadeada/main.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct {
+typedef struct{
     int id;
-    char nome[10];
-    char endereco[200];
-} TipoItem;
+}TipoItem;
 
-typedef struct celula  *apontador;
+typedef struct celula *apontador;
 
 typedef struct celula{
     TipoItem item;
-    apontador prox;
-} celula;
+    apontador *prox;
+}celula;
 
 typedef struct{
-    apontador *primeiro,  *ultimo;
-} TipoLista;
+    apontador primeiro, ultimo;
+}TipoLista;
 
 void criar(TipoLista *lista){
-    lista->primeiro = (apontador*) malloc(sizeof(celula));
+    lista->primeiro = (apontador) malloc(sizeof(celula));
     lista->primeiro->prox = NULL;
     lista->ultimo = lista->primeiro;
 }
@@ -30,15 +28,44 @@ int vazia(TipoLista lista){
     return 0;
 }
 
+void inserirFinal(TipoLista *lista, TipoItem item){
+    lista->ultimo->prox = (apontador) malloc(sizeof(celula));
+    lista->ultimo = lista->ultimo->prox;
+    lista->ultimo->prox = NULL;
+    lista->ultimo->item = item;
+}
+
+void imprimir(TipoLista lista){
+    if(vazia(lista)==1){
+        printf("A lista esta vazia!!!");
+    }else{
+        apontador aux = lista.primeiro->prox;
+        while(aux!=NULL){
+            printf("[%d] ", aux->item.id);
+            aux = aux->prox;
+        }
+    }
+}
+
 int main()
 {
     TipoLista lista;
+    TipoItem item;
 
     criar(&lista);
-    if(vazia(lista) == 1)
-        printf("A lista esta vazia");
-    else
-        printf("A lista NAO esta vazia");
+
+    item.id = 1;
+    inserirFinal(&lista, item);
+
+    item.id = 2;
+    inserirFinal(&lista, item);
+
+    imprimir(lista);
 
     return 0;
 }
+
+
+
+
+
