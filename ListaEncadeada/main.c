24cfd787274ea3b4187d22b2eeec9b27409b71c0
Breno Duarte <brenoduarte239@gmail.com>
2024-03-12 23:05:55 -0300

    Commit

--- ListaEncadeada/main.c
+++ ListaEncadeada/main.c
@@ -29,10 +29,12 @@ int vazia(TipoLista lista){
 }
 
 void inserirFinal(TipoLista *lista, TipoItem item){
-    lista->ultimo->prox = (apontador) malloc(sizeof(celula));
-    lista->ultimo = lista->ultimo->prox;
-    lista->ultimo->prox = NULL;
-    lista->ultimo->item = item;
+    if(buscar(*lista, item.id)==0){
+        lista->ultimo->prox = (apontador) malloc(sizeof(celula));
+        lista->ultimo = lista->ultimo->prox;
+        lista->ultimo->prox = NULL;
+        lista->ultimo->item = item;
+    }
 }
 
 void imprimir(TipoLista lista){
@@ -47,6 +49,36 @@ void imprimir(TipoLista lista){
     }
 }
 
+void remover(TipoLista *lista, int id){
+    if(vazia(*lista)==1){
+        printf("A lista esta vazia!");
+    }else{
+        apontador anterior, atual;
+        anterior = lista->primeiro;
+        atual = lista->primeiro->prox;
+        while(atual!=NULL){
+            if(atual->item.id == id){
+                anterior->prox = atual->prox;
+                free(atual);
+                break;
+            }
+            anterior = atual;
+            atual = atual->prox;
+        }
+    }
+}
+
+int buscar(TipoLista lista, int id){
+    apontador aux;
+    aux = lista.primeiro->prox;
+    while(aux!=NULL){
+        if(aux->item.id == id)
+            return 1;
+        aux = aux->prox;
+    }
+    return 0;
+}
+
 int main()
 {
     TipoLista lista;
@@ -57,10 +89,36 @@ int main()
     item.id = 1;
     inserirFinal(&lista, item);
 
+    item.id = 1;
+    inserirFinal(&lista, item);
+
+    item.id = 1;
+    inserirFinal(&lista, item);
+
     item.id = 2;
     inserirFinal(&lista, item);
 
+    item.id = 3;
+    inserirFinal(&lista, item);
+
+    item.id = 4;
+    inserirFinal(&lista, item);
+
+    printf("Primeira impressao: \n");
     imprimir(lista);
+
+    remover(&lista, 3);
+
+    printf("\n\nSegunda impressao: \n");
+    imprimir(lista);
+
+    printf("\n");
+
+    if(buscar(lista, 2)==1){
+        printf("O ID 2 esta na lista");
+    }else{
+        printf("O ID 2 NAO esta na lista");
+    }
 
     return 0;
 }
