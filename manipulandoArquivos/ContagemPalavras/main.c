d1b81de498a8446c748f07750e068e5fe39f74f5
Breno Duarte <brenoduarte239@gmail.com>
2024-06-03 20:21:14 -0300

    feat: finalizado exercício de contagem de palavras de um arquivo

--- manipulandoArquivos/ContagemPalavras/main.c
+++ manipulandoArquivos/ContagemPalavras/main.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <strings.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    FILE *arquivo, *arquivo_saida;
+    char palavra[50];
+    int contador=0;
+
+    arquivo = fopen("arquivo.txt", "r");
+
+    arquivo_saida = fopen("arquivo_saida.txt", "w");
+
+    if(arquivo==NULL){
+        printf("Erro ao abrir o arquivo");
+        return 1;
+    }
+
+    if(arquivo_saida==NULL){
+        printf("Erro ao abrir o arquivo");
+        return 1;
+    }
+
+    while(fscanf(arquivo, "%s", palavra)==1){
+        contador++;
+    }
+
+    fprintf(arquivo_saida, "Quantidade de palavras: %d", contador);
+
+    fclose(arquivo);
+
+    fclose(arquivo_saida);
+
     return 0;
 }
+
